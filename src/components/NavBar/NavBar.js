35bb9e21dc4081788c60a0fdcfff42efd3b037ea
racssett <rachel.c.hassett@gmail.com>
2022-11-22 23:59:18 -0600

    update styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,16 +10,16 @@ function NavBar() {
       </div>
       <div className={styles.navLinksContainer}>
         <div className={styles.aboutProjectsContainer}>
-          <p className={styles.link}><NavLink to='/about'>About</NavLink></p>
-          <p className={styles.link}><NavLink to='/projects'>Projects</NavLink></p>
+          <p className={styles.about}><NavLink to='/about'>About</NavLink></p>
+          <p className={styles.projects}><NavLink to='/projects'>Projects</NavLink></p>
         </div>
         <div className={styles.namesContainer}>
           <h3 className={styles.name} hidden><NavLink to='/'>Rachel</NavLink></h3>
           <h3 className={styles.name} hidden><NavLink to='/'>Hassett</NavLink></h3>
         </div>
         <div className={styles.resumeContactContainer}>
-          <p className={styles.link}><NavLink to='/resume'>Resume</NavLink></p>
-          <p className={styles.link}><NavLink to='/contact'>Contact</NavLink></p>
+          <p className={styles.resume}><NavLink to='/resume'>Resume</NavLink></p>
+          <p className={styles.contact}><NavLink to='/contact'>Contact</NavLink></p>
         </div>
       </div>
     </nav>
