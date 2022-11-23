6473c3817da88deade112b90a53983b21982c1f3
racssett <rachel.c.hassett@gmail.com>
2022-11-23 00:59:22 -0600

    fix spelling error in about section

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -8,7 +8,7 @@ function About() {
         <img src="/images/Rachel Hassett Picture.jpg" alt="Rachel Hassett" />
         <div className={styles.aboutText}>
           <h2>Who I am</h2>
-          <p> I'm Rachel, a Full-stack w`eb developer with a proclivity for creative problem solving. </p>
+          <p> I'm Rachel, a Full-stack web developer with a proclivity for creative problem solving. </p>
           <h2 className={styles.title}>My Journey</h2>
           <p>I started my tech journey by completing a 420+ hour software engineering immersive through General Assembly. In this immersive, I solved challenges, created projects, and worked together in a team to deliver an app that helps job seekers in the tech world find jobs, events, and resources.</p>
           <h2 className={styles.title}>Current Projects</h2>
